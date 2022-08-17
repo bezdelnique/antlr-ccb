07c032208afeb36b0682d8e3ca01b38f183fcedf
Vitaly Belenky <heman777@yandex.ru>
2022-08-17 10:39:43 +0300

    Some improvements

--- src/test/java/CcbCalculatorInTest.java
+++ src/test/java/CcbCalculatorInTest.java
@@ -10,4 +10,11 @@ public class CcbCalculatorInTest extends BaseCcbCalculatorTest {
         ValueHolder result = evaluate("return[1 in (1,2,3)]");
         assertThat(result.asBoolean()).isEqualTo(Boolean.TRUE);
     }
+
+
+    @Test
+    void whenStringInRangeThenTrue() {
+        ValueHolder result = evaluate("return['heman' in ('olgago', 'veralo', 'heman')]");
+        assertThat(result.asBoolean()).isEqualTo(Boolean.TRUE);
+    }
 }
